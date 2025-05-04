b1684c702619132994c9e645287341121dd06626
Jimmy McBride <mcbride967@gmail.com>
2025-05-04 00:18:25 -0500

    clean up

--- game/scenes/levels/Main.cs
+++ game/scenes/levels/Main.cs
@@ -4,18 +4,7 @@ namespace OpenWorldSurvival.game.scenes.levels;
 
 public partial class Main : Node3D
 {
-    private AnimationTree _swordAnimationTree;
-
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
-    {
-        _swordAnimationTree = GetNode<AnimationTree>("Sword/AnimationTree");
-
-        _swordAnimationTree.Active = false;
-    }
-
-    // Called every frame. 'Delta' has been the elapsed time since the previous frame.
-    public override void _Process(double delta)
     {
     }
 }
